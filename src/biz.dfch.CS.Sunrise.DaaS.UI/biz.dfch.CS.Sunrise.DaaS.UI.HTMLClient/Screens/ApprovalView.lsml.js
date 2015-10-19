5086785241a4aa10470741ead9e452e76fe0d9a4
rufer7 <marc.rufer@d-fens.net>
2015-10-19 17:10:57 +0200

    splitted up into functions

--- src/biz.dfch.CS.Sunrise.DaaS.UI/biz.dfch.CS.Sunrise.DaaS.UI.HTMLClient/Screens/ApprovalView.lsml.js
+++ src/biz.dfch.CS.Sunrise.DaaS.UI/biz.dfch.CS.Sunrise.DaaS.UI.HTMLClient/Screens/ApprovalView.lsml.js
@@ -79,13 +79,7 @@ myapp.ApprovalView.deleteEntity_execute = function (screen) {
 myapp.ApprovalView.goToReferencedObject_execute = function (screen) {
 
     var selectedApproval = screen.Approval;
-    var approvalJobTypeSuffix = "Approval";
-
-    var title = "Loading failed";
-
     var noItemSelectedMessage = "No item selected. Select item and retry operation";
-    var fetchJobFailedMessage = "An error occured while trying to navigate to the object the approval references to";
-    var fetchItemToApproveFailedMessage = "Unable to fetch item to approve";
 
     if (!selectedApproval) {
         msls.showMessageBox(noItemSelectedMessage, {
@@ -93,70 +87,83 @@ myapp.ApprovalView.goToReferencedObject_execute = function (screen) {
             buttons: msls.MessageBoxButtons.ok
         });
     } else {
-
-        // Get job of selected approval
-        var filter = "(endswith(Name, " + msls._toODataString(approvalJobTypeSuffix, ":String") + ")" + " and ReferencedItemId eq " + msls._toODataString(selectedApproval.Id, ":String") + ")";
-        myapp.activeDataWorkspace.CoreData.Jobs.filter(filter).execute().then(function (result) {
-                var approvalJob = result.results[0];
-                var parentJobId = approvalJob.ParentId;
-
-                // Get parent job of approval job
-                var filter2 = "(Id eq " + parentJobId + ")";
-                myapp.activeDataWorkspace.CoreData.Jobs.filter(filter2).execute().then(function (result) {
-                    var parentJob = result.results[0];
-
-                    var n = parentJob.Name.lastIndexOf('.');
-                    var entityType = parentJob.Name.substring(n + 1);
-
-                    // Get item to approve
-                    var filter3 = "(Id eq " + parseInt(parentJob.ReferencedItemId) + ")";
-                    myapp.activeDataWorkspace.CoreData[entityType + "s"].filter(filter3).execute().then(function (result) {
-                        var itemToApprove = result.results[0];
-                        myapp["show" + entityType + "View"](itemToApprove);
-                    },
-                    function fail(e) {
-                        msls.showMessageBox(
-                            fetchItemToApproveFailedMessage
-                            ,
-                            {
-                                title: title
-                                ,
-                                buttons: msls.MessageBoxButtons.ok
-                            }).then(function (result) {
-                                myapp.cancelChanges();
-                                throw e;
-                            });
-                    });
-                },
-                function fail(e) {
-                    msls.showMessageBox(
-                        fetchJobFailedMessage
-                        ,
-                        {
-                            title: title
-                            ,
-                            buttons: msls.MessageBoxButtons.ok
-                        }).then(function (result) {
-                            myapp.cancelChanges();
-                            throw e;
-                        });
-                });
-        },
-        function fail(e) {
-            msls.showMessageBox(
-                fetchJobFailedMessage
-                ,
-                {
-                    title: title
-                    ,
-                    buttons: msls.MessageBoxButtons.ok
-                }).then(function (result) {
-                    myapp.cancelChanges();
-                    throw e;
-                });
-        });
+        navigateToItemToApprove(screen);
     }
 };
+
+function navigateToItemToApprove(screen) {
+
+    var selectedApproval = screen.Approval;
+    var approvalJobTypeSuffix = "Approval";
+
+    // Get job of selected approval
+    var filter = "(endswith(Name, " + msls._toODataString(approvalJobTypeSuffix, ":String") + ")" + " and ReferencedItemId eq " + msls._toODataString(selectedApproval.Id, ":String") + ")";
+    myapp.activeDataWorkspace.CoreData.Jobs.filter(filter).execute().then(function (result) {
+        var approvalJob = result.results[0];
+        var parentJobId = approvalJob.ParentId;
+
+        // Get parent job of approval job
+        var parentJobFilter = "(Id eq " + parentJobId + ")";
+        myapp.activeDataWorkspace.CoreData.Jobs.filter(parentJobFilter).execute().then(function (result) {
+            var parentJob = result.results[0];
+
+            getItemToApprove(parentJob, function callback(itemToApprove, entityType) {
+                myapp["show" + entityType + "View"](itemToApprove);
+            });            
+            },
+        onJobLoadingFailed
+        );
+    },
+    onJobLoadingFailed);
+};
+
+function onJobLoadingFailed(e) {
+    var title = "Loading failed";
+    var fetchJobFailedMessage = "An error occured while trying to navigate to the object the approval references to";
+
+    msls.showMessageBox(
+        fetchJobFailedMessage
+        ,
+        {
+            title: title
+            ,
+            buttons: msls.MessageBoxButtons.ok
+        }).then(function (result) {
+            myapp.cancelChanges();
+            throw e;
+        });
+}
+
+function getItemToApprove(parentJob, resultCallback) {
+    var n = parentJob.Name.lastIndexOf('.');
+    var entityType = parentJob.Name.substring(n + 1);
+
+    var filter = "(Id eq " + parseInt(parentJob.ReferencedItemId) + ")";
+
+    myapp.activeDataWorkspace.CoreData[entityType + "s"].filter(filter).execute().then(function (result) {
+            var itemToApprove = result.results[0];
+            resultCallback(itemToApprove, entityType);
+        },
+    onItemToApproveLoadingFailed
+    );
+}
+
+function onItemToApproveLoadingFailed(e) {
+    var title = "Loading failed";
+    var fetchItemToApproveFailedMessage = "Unable to fetch item to approve";
+
+    msls.showMessageBox(
+        fetchItemToApproveFailedMessage
+        ,
+        {
+            title: title
+            ,
+            buttons: msls.MessageBoxButtons.ok
+        }).then(function (result) {
+            myapp.cancelChanges();
+            throw e;
+        });
+}
 
 /**
  * Copyright 2015 d-fens GmbH
